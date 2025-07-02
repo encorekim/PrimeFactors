b18935e306849f2d28eb1c635239c39a5c1a54dc
encorekim <rlawodus95@hanmail.net>
2025-07-02 10:13:23 +0900

    [refactor] 46 Add Fixture to unit test
    - Add Fixture to unit test

--- primefactor/main.cpp
+++ primefactor/main.cpp
@@ -1,14 +1,19 @@
 #include "gmock/gmock.h"
 #include "primefactors.cpp"
 
-TEST(PrimeFactors, Of1) {
+using namespace testing;
+
+class PrimeFactorsFixture : public Test {
+ public:
   PrimeFactors prime;
+};
+
+TEST_F(PrimeFactorsFixture, Of1) {
   vector<int> expected{};
   EXPECT_EQ(expected, prime.getPrimeFactors(1));
 }
 
-TEST(PrimeFactors, Of2) {
-  PrimeFactors prime;
+TEST_F(PrimeFactorsFixture, Of2) {
   vector<int> expected{2};
   EXPECT_EQ(expected, prime.getPrimeFactors(2));
 }
