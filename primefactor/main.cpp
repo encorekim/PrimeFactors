a820410e0382205414da91712f95777b4500d26e
encorekim <rlawodus95@hanmail.net>
2025-07-02 10:05:09 +0900

    [feature] 41 Add PrimeFactors Class
        - Change name; primefactor.cpp into primefactors.cpp
        - Add PrimFactors class and temporary function

--- primefactor/main.cpp
+++ primefactor/main.cpp
@@ -1,4 +1,11 @@
 #include "gmock/gmock.h"
+#include "primefactors.cpp"
+
+TEST(PrimeFactors, Of1) {
+  PrimeFactors prime;
+  vector<int> expected{};
+  EXPECT_EQ(expected, prime.of(1));
+}
 
 int main() {
   ::testing::InitGoogleTest();
